71dab499b0517190bb4bd0ce18eef234308d5bfb
mayboyxxx <865939178@qq.com>
2019-01-20 23:19:04 +0800

    svm1 test
    只有一个svm文件  其他都是虚拟环境文件venv

---
+++ MachineL4Sec/svm01.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+#coding=utf-8
+from sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
+from sklearn.svm import SVR
+from sklearn.datasets import load_boston
+boston = load_boston()
+print(boston.DESCR)
